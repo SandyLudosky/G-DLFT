3a9120994205293d52c7d54b7ad137356a8aafc2
Sandy Ludosky <sludosky@xebia.fr>
2023-08-11 17:51:48 +0200

    BUG: Clubs should not be able to use more than their points allowed

--- server.py
+++ server.py
@@ -51,14 +51,21 @@ def book(competition, club):
                                competitions=competitions)
 
 
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
     competition = [c for c in competitions
                    if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
+    if int(club['points']) < placesRequired :
+        flash("You do not have enough points!")
+    elif int(competition['numberOfPlaces']) < placesRequired :
+        flash("SOLD OUT!")
+    else:
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+        club['points'] = int(club['points']) - placesRequired
+        flash('Great-booking complete!')
+
     return render_template('welcome.html', club=club,
                            competitions=competitions)
 
